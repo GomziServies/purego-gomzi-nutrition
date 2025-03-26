47060852ba39a96817da9b0d02e5e2caf11176fd
Yash Sutariya <steelbird171@gmail.com>
2025-03-26 18:02:45 +0530

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1512,7 +1512,7 @@ function Home() {
             </div>
           </div>
         </section>
-        <section id="news" className="tg-blog-area pt-120 pb-90">
+        <section id="news" className="tg-blog-area pt-120">
           <div className="container">
             <div className="row justify-content-center">
               <div className="col-xl-6 col-lg-8">
