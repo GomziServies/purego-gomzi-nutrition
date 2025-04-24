40a207cf6051c389b452c73048550cea043c73fd
Yash Sutariya <84495766+yash-sutariya@users.noreply.github.com>
2025-04-24 15:30:44 +0530

    Update MoreProduct.jsx

--- src/components/MoreProduct.jsx
+++ src/components/MoreProduct.jsx
@@ -133,9 +133,9 @@ const MoreProduct = () => {
       <section className="features-products">
         <div className="section-title text-center mb-60">
           <p className="sub-title">.. Increased Wellness With Purego ..</p>
-          <h1 className="title">Purego Products</h1>
+          <h2 className="title">Purego Products</h2>
         </div>
-        <div className="container text-center">
+        <div className="container">
           {/* justify-content-center */}
           <div className="row">
             <OwlCarousel {...options} className="owl-theme d-block">
