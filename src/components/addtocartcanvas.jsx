5e66c7d0925f901d21e99c95d63b45133fafda3e
Yash Sutariya <84495766+yash-sutariya@users.noreply.github.com>
2025-05-30 16:36:47 +0530

    Update addtocartcanvas.jsx

--- src/components/addtocartcanvas.jsx
+++ src/components/addtocartcanvas.jsx
@@ -793,15 +793,11 @@ const AddtoCartOffCanvas = ({
                 data-dismiss="modal"
                 aria-label="Close"
               >
-                <svg
-                  tabIndex="0"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 329.26933 329"
-                  width="16px"
-                  height="12px"
-                >
-                  <path d="m194.800781 164.769531 128.210938-128.214843c8.34375-8.339844 8.34375-21.824219 0-30.164063-8.339844-8.339844-21.824219-8.339844-30.164063 0l-128.214844 128.214844-128.210937-128.214844c-8.34375-8.339844-21.824219-8.339844-30.164063 0-8.34375 8.339844-8.34375 21.824219 0 30.164063l128.210938 128.214843-128.210938 128.214844c-8.34375 8.339844-8.34375 21.824219 0 30.164063 4.15625 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921875-2.089844 15.082031-6.25l128.210937-128.214844 128.214844 128.214844c4.160156 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921874-2.089844 15.082031-6.25 8.34375-8.339844 8.34375-21.824219 0-30.164063zm0 0"></path>
-                </svg>
+                <img
+                  alt="product"
+                  className="img-fluid cp"
+                  src={process.env.PUBLIC_URL + "/assets/images/close.png"}
+                />
               </span>
             </div>
           </div>
@@ -1059,9 +1055,6 @@ const AddtoCartOffCanvas = ({
                                         onClick={() => {
                                           setManualCouponCode(offer.code);
                                           setShowCouponModal(false);
-                                          // handleApplyClick(offer, {
-                                          //   AutoPromoCode: true,
-                                          // });
                                           handleApplyClick(offer.code);
                                           setAppliedCouponCode(offer.code);
                                         }}
@@ -1210,7 +1203,7 @@ const AddtoCartOffCanvas = ({
                                   <div className="d-flex mx-2 justify-content-between">
                                     <div className="col-12">
                                       <h2
-                                        className="f-rob-bol d-inline-block h3-fs cp mb-2 fs-18"
+                                        className="f-rob-bol d-inline-block text-capitalize h3-fs cp mb-2 fs-18"
                                         title="Everyday Sweet | 1:1 Sugar Replacer | Zero Calories | 100% Natural"
                                       >
                                         {suggestionProduct.name}
