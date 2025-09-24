820cd88f9d9b513dbe48bedfc35ce117ae62e824
Yash Sutariya <84495766+yash-sutariya@users.noreply.github.com>
2025-09-24 13:12:57 +0530

    Update thankYou.jsx

--- src/components/thankYou.jsx
+++ src/components/thankYou.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import NutritionHeader from "./partials/Header/nutritionsheader";
 import { useState } from "react";
 import HomeNutritionFooter from "./partials/Footer/footer";
+import { Helmet } from "react-helmet";
 
 const ThankYou = () => {
     const [clickATC, setClickATC] = useState(false);
@@ -11,6 +12,27 @@ const ThankYou = () => {
     };
     return (
         <>
+            <Helmet>
+                <script>
+                    { `
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '3713420712287031');
+                    fbq('track', 'PageView');
+                `}
+                </script>
+                <noscript>
+                    { `<img height="1" width="1" style="display:none"
+                src="https://www.facebook.com/tr?id=3713420712287031&ev=PageView&noscript=1"
+                />`}
+                </noscript>
+            </Helmet>
             <NutritionHeader handleCartOpen={handleCartOpen} />
             <section className="marginbottom-nutrition py-5 my-5">
                 <div className="container-fluid pt-md-5">
